14d89629ed789778a73ea599a95af58df93d06dd
ay0530 <edkdud530@gmail.com>
2024-04-22 22:27:40 +0900

    💡 comment: 네이버웹툰 주석 작성중

--- src/crawler/naver/crawlFinishedNaverWebtoons.ts
+++ src/crawler/naver/crawlFinishedNaverWebtoons.ts
@@ -3,23 +3,30 @@ import { getHtmlData } from './getHtmlData';
 import { NAVER_WEBTOON_URL } from '.';
 import { UpdateDay, Webtoon } from '../../types';
 
+/** 완결 웹툰 크롤링 */
 export const crawlFinishedNaverWebtoons = async () => {
+  // 페이지 링크
   const $ = await getHtmlData(NAVER_WEBTOON_URL + '/webtoon/finish.nhn?page=1');
+
+  // 총 페이지 수
   const pageCount = Number(
     $('#ct > div.section_list_toon > div.paging_type2 > em > span').text(),
   );
 
+  // Array.from(유사 배열 객체, 값) : 유사 배열 또는 이터러블 객체를 새 배열로 변환
   const pageList = Array.from({ length: pageCount }, (_, i) => i + 1);
 
+  // 배열 초기화
   const finishedWebtoons: Webtoon[] = [];
 
-  //* 각 페이지를 비동기적으로 크롤링
+  // 각 페이지를 비동기적으로 크롤링
   await Promise.all(
     pageList.map(async (page) => {
+      // 네이버 웹툰 크롤링
       const webtoonsOfPage = await crawlNaverWebtoons(
-        'finish',
-        `page=${page}`,
-        [UpdateDay.FINISHED],
+        'finish', // type
+        `page=${page}`, // query
+        [UpdateDay.FINISHED], // updateDays
       );
       finishedWebtoons.push(...webtoonsOfPage);
     }),
--- src/crawler/naver/crawlNaverWebtoons.ts
+++ src/crawler/naver/crawlNaverWebtoons.ts
@@ -3,20 +3,25 @@ import { getHtmlData } from './getHtmlData';
 import { Webtoon, Singularity, ServiceCode, Service } from '../../types';
 import { standardizeChars } from 'utils';
 
+/** 네이버 웹툰 크롤링 */
 export async function crawlNaverWebtoons(
   type: 'weekday' | 'finish',
   query: string,
   updateDays: Webtoon['updateDays'],
   commonSingularityList: Singularity[] = [],
 ) {
+  // 페이지 링크
   const $ = await getHtmlData(
     `${NAVER_WEBTOON_URL}/webtoon/${type}.nhn?${query}`,
   );
 
+  // 웹툰 정보
   const rootElement = $('#ct > div.section_list_toon > ul > li > a');
 
+  // 웹툰 정보 반환
   return rootElement
     .map((_, element) => {
+      // 뱃지 여부
       const badgeAreaText = $(element).find('span.area_badge').text();
 
       const isNewWebtoon = badgeAreaText.includes('신작');
@@ -28,37 +33,50 @@ export async function crawlNaverWebtoons(
         singularityList.push(Singularity.WAIT_FREE);
       }
 
+      // 업데이트 현황
       const titleBoxText = $(element).find('div.title_box').text();
       const isPausedWebtoon = titleBoxText.includes('휴재');
       const isUpdatedWebtoon = titleBoxText.includes('업데이트');
 
+      // 관심 수
       const fanCountText = $(element)
         .find('div.info > span.favcount > span.count_num')
         .text();
 
+      // 천까지 해도 되지 않을까 ? - 아영
       const fanCount = fanCountText.includes('만')
         ? Number(fanCountText.replace('만', ''))
         : fanCountText.includes('억')
         ? Number(fanCountText.replace('억', '')) * 10000
         : null;
 
+      // 웹툰 상세 정보 주소
       const path = $(element).attr('href');
 
+      // 웹툰 제목
       const title = $(element).find('.title').text();
 
+      // 웹툰 작가
       const author = $(element)
         .find('.author')
         .text()
-        .replaceAll(' / ', ',')
-        .replaceAll('\n', '')
-        .replaceAll('\t', '');
+        // replaceAll과 replace의 속도 차이가 있을까 ?
+        // .replaceAll(' / ', ',')
+        // .replaceAll('\n', '')
+        // .replaceAll('\t', '');
+        .replace(' / ', ',')
+        .replace(/\n|\t/g, '');
 
+      // 웹툰 번호
+      // 웹툰 ID랑 타이틀 ID랑 다른거 ???? - 아영
       const titleId = path
         ? Number(path.split('?titleId=')[1].split('&')[0])
         : 0;
+      console.log('titleId: ', titleId);
 
+      // 웹툰 정보 정리
       const webtoon: Webtoon = {
-        webtoonId: ServiceCode.NAVER + titleId,
+        webtoonId: ServiceCode.NAVER + titleId, // 서비스코드 추가하는 이유가 뭐임 ? 카카오 네이버 구분인가 굳이 해야할 필요가 있나 ? service 필드가 잇잔아 - 아영
 
         title,
 
